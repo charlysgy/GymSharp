34ac80e0e0e7cb3615659b30dd297afa930ab1c9
LeProDeLaBosse <hugoviala57@gmail.com>
2022-03-03 16:39:08 +0100

    enums et msg accueil

--- GymSharp/ressources/enums/muscles_enum.cs
+++ GymSharp/ressources/enums/muscles_enum.cs
@@ -8,6 +8,17 @@ namespace GymSharp.ressources.enums
 {
     public enum Muscles 
     {
-
+        Pectoraux = 1,
+        Dos = 2,
+        Biceps = 3,
+        Triceps = 4,
+        Epaules = 5,
+        Abdominaux = 6,
+        Quadirceps = 7,
+        Ischios = 8,
+        Mollets = 9,
+        Avant_bras = 10,
+        Bras = 11,
+        Jambes = 12
     }
 }

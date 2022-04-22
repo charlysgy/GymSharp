43ae3e88987a6fd965e78e2ce15e6302e56fcc00
LeProDeLaBosse <hugoviala57@gmail.com>
2022-04-22 23:53:19 +0200

    rectifications

--- GymSharp/MVVM/View/ProfileView.xaml.cs
+++ GymSharp/MVVM/View/ProfileView.xaml.cs
@@ -25,6 +25,12 @@ namespace GymSharp.MVVM.View
     public partial class ProfileView : UserControl
     {
         UserProfile profile = new UserProfile();
+        int cc;
+        int rm;
+        int rmObj;
+        int crypt;
+        string sex;
+        int weightObj;
         public ProfileView()
         {
             InitializeComponent();
@@ -73,7 +79,61 @@ namespace GymSharp.MVVM.View
 
         private void WeightBoxProfile_TouchEnter(object sender, TouchEventArgs e)
         {
-            profile.weight = Int32.Parse(FirstNameBox.Text);
+            profile.weight = Int32.Parse(WeightBox.Text);
+        }
+        public string GetInfoCC()
+        {
+            return CCBoxProfile.SelectedText;
+        }
+
+        private void CCBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            cc = Int32.Parse(RMBoxProfile.Text);
+        }
+        public string GetInfoCrypt()
+        {
+            return CryptBoxProfile.SelectedText;
+        }
+
+        private void CryptBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            crypt = Int32.Parse(CryptBoxProfile.Text);
+        }
+        public string GetInfoSex()
+        {
+            return SexBoxProfile.SelectedText;
+        }
+
+        private void SexBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            sex = SexBoxProfile.Text;
+        }
+        public string GetInfoRMObj()
+        {
+            return RMObjBoxProfile.SelectedText;
+        }
+
+        private void RMObjBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            rmObj = Int32.Parse(RMObjBoxProfile.Text);
+        }
+        public string GetInfoWeightObj()
+        {
+            return WeightObjBoxProfile.SelectedText;
+        }
+
+        private void WeightObjBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            weightObj = Int32.Parse(WeightObjBoxProfile.Text);
+        }
+        public string GetInfoRM()
+        {
+            return RMBoxProfile.SelectedText;
+        }
+
+        private void RMBoxProfile_TouchEnter(object sender, TouchEventArgs e)
+        {
+            rm = Int32.Parse(RMBoxProfile.Text);
         }
     }
 }

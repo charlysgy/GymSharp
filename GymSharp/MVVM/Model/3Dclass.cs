7ca9398a12cfb3d28d534e87754cd757a2a4a2b5
Charly Saugey <charly.sauugey@gmail.com>
2022-06-01 10:58:56 +0200

    Jeu soui da quor
    (Ajout du corps en 3D ébauche seulement)

---
+++ GymSharp/MVVM/Model/3Dclass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using GymSharp.ressources.enums;
+
+namespace GymSharp.MVVM.Model
+{
+    public static class _3Dclass
+    {
+        private static List<List<int>> _face = new List<List<int>>();
+        private static List<List<int>> _back = new List<List<int>>();
+        private static List<List<int>> _left = new List<List<int>>();
+        private static List<List<int>> _right = new List<List<int>>();
+        public static Muscles CoordsToMuscle(Point O, double winx, double winy, string side)
+        {
+            return Muscles.Abdominaux;
+        }
+
+        private static double GetAngle(Point A, Point B, Point C)
+        {
+            double angle = 0;
+
+            return angle;
+        }
+    }
+}
